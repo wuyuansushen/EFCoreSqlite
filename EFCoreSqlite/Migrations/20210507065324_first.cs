5b0d8b1b6a3b1b07b4f8ea2beba391ba75ce9836
wuyuansushen <wuyuansushen@gmail.com>
2021-05-07 19:17:25 +0800

    Add project files.

---
+++ EFCoreSqlite/Migrations/20210507065324_first.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EFCoreSqlite.Migrations
+{
+    public partial class first : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Blogs",
+                columns: table => new
+                {
+                    BlogID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Url = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Blogs", x => x.BlogID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Posts",
+                columns: table => new
+                {
+                    PostID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Title = table.Column<string>(type: "TEXT", nullable: true),
+                    Content = table.Column<string>(type: "TEXT", nullable: true),
+                    BlogID = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Posts", x => x.PostID);
+                    table.ForeignKey(
+                        name: "FK_Posts_Blogs_BlogID",
+                        column: x => x.BlogID,
+                        principalTable: "Blogs",
+                        principalColumn: "BlogID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Posts_BlogID",
+                table: "Posts",
+                column: "BlogID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Posts");
+
+            migrationBuilder.DropTable(
+                name: "Blogs");
+        }
+    }
+}
---
+++ EFCoreSqlite/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace EFCoreSqlite
+{
+    class Program
+    {
+        public class BloggingContext:DbContext
+        {
+            public DbSet<Blog> Blogs { get; set; }
+            public DbSet<Post> Posts { get; set; }
+
+            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            {
+                //Warning: Build binary file isn't exported in current directory.
+                optionsBuilder.UseSqlite(@"Data Source=./../../../blogging.db");
+            }
+        }
+
+        public class Blog
+        {
+            public int BlogID { get; set; }
+            public string Url { get; set; }
+            public List<Post> Posts { get; } = new List<Post>();
+        }
+
+        public class Post
+        {
+            public int PostID { get; set; }
+            public string Title { get; set; }
+            public string Content { get; set; }
+            public int BlogID { get; set; }
+        }
+        static void Main(string[] args)
+        {
+            using var db = new BloggingContext();
+
+            Console.WriteLine("Inserting...");
+            db.Add(new Blog { Url = "https://github.com" });
+            db.SaveChanges();
+
+            Console.WriteLine("Querying first entity...");
+            var blog = db.Blogs.OrderBy(b => b.BlogID).First();
+            blog.Url = @"https://cn.bing.com/";
+            blog.Posts.Add(new Post { Title = "First Post", Content = @"First Post information." });
+            db.SaveChanges();
+            db.Remove(blog);
+            db.SaveChanges();
+        }
+    }
+}
